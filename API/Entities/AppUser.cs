4707d165665b5a9460be0d912575a8e424655ab9
ERIC <xufei2012@gmail.com>
2023-09-24 15:29:33 +1300

    update the defalt connection string value of the db,and created the first db file

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,10 +1,8 @@
-using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities;
 
 public class AppUser
 {
-    [Key]
     public int Id { get; set; }
     public string UserName { get; set; }
 
