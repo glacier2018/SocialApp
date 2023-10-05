bc98b6f1176525f9120d8dbed7ffdb3d6fe9e440
ERIC <45778515+glacier2018@users.noreply.github.com>
2023-10-05 20:13:39 +1300

    intall ngx-bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   title = 'client';
+  users: any;
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => {
+        this.users = res;
+      },
+
+      error: (err) => console.log(err),
+    });
+  }
 }
